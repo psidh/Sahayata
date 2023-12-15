69534dad233ffae367bc220cce188973b38c2ae2
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-12-15 21:54:46 +0530

    Loading.tsx added to all routes
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='min-h-screen flex flex-col justify-center items-center'>
+      <img src="/loading.gif" alt="Loading GIF" className='w-1/2 h-1/2' />
+    </div>
+  )
+}
