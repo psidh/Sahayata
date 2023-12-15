34dc852cbb7656b79a394b33713ef36694e895e6
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-12-15 21:11:43 +0530

    Sidebar UI updated
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

---
+++ src/app/exp/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import Material from '../../components/material'
+
+export default function page() {
+  return (
+    <div>
+      helo
+      <Material />
+      <div>
+        Hzelllo
+      </div>
+    </div>
+  )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import Link from 'next/link';
 import { AiOutlineMenu } from "react-icons/ai";
+import { RiHome2Line } from "react-icons/ri";
 export default function Navbar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
@@ -13,11 +14,11 @@ export default function Navbar() {
   =`py-1 rounded-full bg-blue-800 px-6 text-white border-2 hover:text-black
     hover:bg-white border-blue-800 transition duration-300`
 
-  const normal ='text-lg hover:text-[#1900ff] mx-4 my-2 md:m-0';
+  const normal ='text-md hover:text-[#1900ff] mx-4 my-2 md:m-0';
 
   return (
     <div>
-      <div className="bg-white p-10 backdrop-blur-md bg-opacity-20 flex justify-between">
+      <div className=" bg-white p-6 backdrop-blur-md bg-opacity-20 flex justify-between">
         <div className='flex justify-between items-center space-x-8'>
         <img src="/azadi.png" alt="" className='w-20 h-auto'/>
         <img src="/gl.png" alt="" className='w-20 h-auto'/>
@@ -25,9 +26,9 @@ export default function Navbar() {
         <div className="space-x-8 hidden lg:flex justify-between items-center">
           <Link href={'/'} className={normal}>Home</Link>
           <Link href={'/about'} className={normal}>About</Link>
-          <Link href={'/contact'} className={normal}>Contact</Link>
-          <Link href={'/login'} className={linkClass}>Login</Link>
+          <Link href={'/contact'} className={normal}>Contact</Link>          
           <Link href={'/dashboard'} className={linkClass}>Dashboard</Link>
+          <Link href={'/login'} className={linkClass}>Login</Link>
           <Link href={'/signup'} className={linkClass}>SignUp</Link>     
         </div>
         <AiOutlineMenu onClick={toggleNav}  className="flex lg:hidden w-6 h-6 md:w-12 md:h-12 cursor-pointer"/>
@@ -38,8 +39,8 @@ export default function Navbar() {
           <Link href={'/'} className={normal}>Home</Link>
           <Link href={'/about'} className={normal}>About</Link>
           <Link href={'/contact'} className={normal}>Contact</Link>
-          <Link href={'/login'} className={linkClass}>Login</Link>
           <Link href={'/dashboard'} className={normal}>Dashboard</Link>
+          <Link href={'/login'} className={normal}>Login</Link>
           <Link href={'/signup'} className={normal}>SignUp</Link>    
         </div>
       )}
