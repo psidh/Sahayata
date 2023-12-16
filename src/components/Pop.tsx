778f93cccc8f6eda37629e634d852ab391ce46a4
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-12-16 18:40:27 +0530

    Pop Up updated
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/components/Pop.tsx
+++ src/components/Pop.tsx
@@ -7,7 +7,7 @@ interface PopupItem {
   currentCapacity: number;
   availableCapacity: number;
   operatorId: string;
-  time : string;
+  time: string;
 }
 
 interface PopupProps {
@@ -23,26 +23,42 @@ const Popup: React.FC<PopupProps> = ({ onClose, item }) => {
         onClick={onClose}
       ></div>
 
-      <div className="bg-white py-6 px-16 rounded-lg z-10 flex-col relative  flex items-center justify-center">
-        <div className="my-4 text-2xl">
-          <h1 className=" font-bold my-4">{item.dumperId}</h1>
-          <p className="text-gray-600 my-4">Date: {item.date}</p>
-          <p
-            className={`${
-              item.status === 'isfilled' ? 'text-green-500' : 'text-red-500 '
-            }  font-semibold my-4`}
-          >
-            Status: {item.status}
-          </p>
-
-          <p className="text-gray-600 my-4">
-            Current Capacity: {item.currentCapacity}
-          </p>
-          <p className="text-gray-600 my-4">
-            Available Capacity: {item.availableCapacity}
-          </p>
-          <p className="text-gray-600 mt-4">Operator ID: {item.operatorId}</p>
-          <p className="text-gray-600 mt-4">Time: {item.time}</p>
+      <div className="bg-white py-6 w-[50%] px-8 rounded-lg  z-10 flex-col relative ">
+        <div className="my-4 flex-col text-2xl">
+          <div className='flex justify-between items-center'>
+            <p className='my-4 font-semibold'>Date</p>
+            <p className='my-4  py-2  px-4 rounded-xl '>{item.date}</p>
+          </div>
+          <div className='flex justify-between items-center'>
+            <p className='my-4 font-semibold'>Dumper ID</p>
+            <p className='my-4  py-2  px-4 rounded-xl '>{item.dumperId}</p>
+          </div>
+          <div className='flex justify-between items-center'>
+            <p className='my-4 font-semibold'>Status</p>
+            <p
+              className={`${
+                item.status === 'isfilled' ? 'text-green-500   py-2  px-4 rounded-xl ' : 'text-red-500   py-2  px-4 rounded-xl '
+              }  font-semibold my-4`}
+            >
+              {item.status}
+            </p>
+          </div>
+          <div className='flex justify-between items-center'>
+            <p className='my-4 font-semibold'>Current Capacity</p>
+            <p className='my-4  py-2  px-4 rounded-xl '>{item.currentCapacity}</p>
+          </div>
+          <div className='flex justify-between items-center'>
+            <p className='my-4 font-semibold'>Available Capacity</p>
+            <p className='my-4  py-2  px-4 rounded-xl '>{item.availableCapacity}</p>
+          </div>
+          <div className='flex justify-between items-center'>
+            <p className='my-4 font-semibold'>Operator ID</p>
+            <p className="my-4  py-2  px-4 rounded-xl ">{item.operatorId}</p>
+          </div>
+          <div className='flex justify-between items-center'>
+            <p className="my-4 font-semibold">Time </p>
+            <p className='my-4  py-2  px-4 rounded-xl '> {item.time}</p>
+          </div>
         </div>
         <button
           className="relative bg-red-500 mt-6 w-full text-white py-2 px-8 rounded-md hover:bg-red-600"
@@ -50,7 +66,6 @@ const Popup: React.FC<PopupProps> = ({ onClose, item }) => {
         >
           Close
         </button>
-        {/* Additional details or content can be added here */}
       </div>
     </div>
   );
