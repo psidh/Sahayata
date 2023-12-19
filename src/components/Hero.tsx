6910f4b37b6238310947f5f2955a2202931ba365
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-12-19 14:48:29 +0530

    about chahnged

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -32,48 +32,49 @@ export default function Hero() {
   };
 
   return (
-    <div className="bg-cover bg-top h-screen bg-[url('/home.jpg')]">
-      <div className='flex '>
+    <div className="bg-cover bg-top pb-40 bg-[url('/home.jpg')]">
+      <div className="flex ">
         <div className="p-8">
-          <div className="p-8 backdrop-lg bg-opacity-80 bg-white w-[45%] rounded-xl">
+          <div className="p-8 backdrop-lg bg-opacity-80 bg-white  rounded-xl">
             <div>
               <h1 className="text-4xl font-semibold px-4 py-2">What We Do</h1>
-              <p className="leading-10 text-justify px-4 py-2 text-xl">
-                Specialize in delivering cutting-edge real-time monitoring
-                solutions. Focus on dumper load status through intuitive
-                visualization and proactive alerts. Provide a seamlessly
-                integrated platform for smooth data transmission. Ensure
-                compatibility across diverse systems and scalability. Fortify
-                solutions with robustness for reliability. Elevate user
-                experience through professional and reliable solutions.
-                Prioritize security and privacy in all aspects of our services.
-                Offer comprehensive monitoring and management of dumper
-                operations.
-              </p>
+              <ul className="list-disc leading-10 flex flex-col text-justify px-4 py-2 text-xl">
+                <li>Real-time monitoring, prioritizing dumper load status</li>
+                <li>Seamless integration for data transmission</li>
+                <li>Compatibility across systems with scalability</li>
+                <li>Robust solutions for reliability</li>
+                <li>Professional, reliable user experience</li>
+                <li>Emphasis on security and privacy</li>
+                <li>Comprehensive dumper operations monitoring</li>
+              </ul>
             </div>
           </div>
         </div>
         <div>
-          {text.map((faq) => (
-            <div key={faq.id} className="my-4">
-              <div
-                className="flex justify-between items-center cursor-pointer"
-                onClick={() => toggleItem(faq.id)}
-              >
-                <h3 className="text-xl font-semibold text-black">{faq.name}</h3>
-                <span
-                  className={`transition-transform transform duration-300 ${
-                    openItems.includes(faq.id) ? 'rotate-180' : ''
-                  }`}
+          <div className='ml-24 w-[25%]]'>
+            {text.map((faq) => (
+              <div key={faq.id} className="my-4">
+                <div
+                  className="flex justify-between bg-white rounded-full cursor-pointer"
+                  onClick={() => toggleItem(faq.id)}
                 >
-                  ▼
-                </span>
+                  <h3 className="text-xl font-semibold text-black">
+                    {faq.name}
+                  </h3>
+                  <span
+                    className={`transition-transform transform duration-300 ${
+                      openItems.includes(faq.id) ? 'rotate-180' : ''
+                    }`}
+                  >
+                    ▼
+                  </span>
+                </div>
+                {openItems.includes(faq.id) && (
+                  <p className="my-2 text-black">{faq.description}</p>
+                )}
               </div>
-              {openItems.includes(faq.id) && (
-                <p className="my-2 text-black">{faq.description}</p>
-              )}
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
