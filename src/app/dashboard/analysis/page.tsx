1beb2ee8b883660002b83b159e56b389c11b3615
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-03-27 22:41:55 +0530

    removed tableConfig.ts

--- src/app/dashboard/analysis/page.tsx
+++ src/app/dashboard/analysis/page.tsx
@@ -1,11 +1,5 @@
-'use client';
 import BarGraph from '@/components/BarGraph';
-import React, { useState } from 'react';
 import Sidebar from '@/components/Sidebar';
-import Popup from '@/components/Pop';
-import Image from 'next/image';
-import Second from '@/components/Second';
-import Third from '@/components/Third';
 
 export default function Page() {
   return (
@@ -16,8 +10,8 @@ export default function Page() {
       <div className="w-[75%]">
         <div className="flex flex-col">
           <BarGraph />
-          <Third />
-          <Second />
+          {/* <Third /> */}
+          {/* <Second /> */}
         </div>
       </div>
     </div>
