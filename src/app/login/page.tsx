fbdac3dcc6f23cb727fa04140a29b00d8a92d3f1
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-03-27 23:43:12 +0530

    error.message added in the page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,7 +23,7 @@ export default function Login() {
     try {
       setLoading(true);
       toast.loading('Waiting...', {
-        duration: 2000, 
+        duration: 2000,
       });
       console.log(user);
       const response = await axios.post('/api/users/login', user);
@@ -34,7 +34,7 @@ export default function Login() {
     } catch (error: any) {
       toast.error('Email or Password seems to be incorrect. Please try again');
       console.log('Login Failed');
-      console.log(error);
+      console.log(error.message);
     } finally {
       setLoading(false);
     }
@@ -51,59 +51,61 @@ export default function Login() {
   return (
     <div>
       <Toaster />
-      <div className="flex flex-col h-fit py-24 justify-center items-center bg-body">
+      <div className='flex flex-col h-fit py-24 justify-center items-center bg-body'>
         <div
-          className="p-5 md:py-6 md:px-16 w-[80%] md:w-[40%] backdrop-xl 
+          className='p-5 md:py-6 md:px-16 w-[80%] md:w-[40%] backdrop-xl 
        bg-opacity-80 border bg-white border-gray-200 rounded-xl
-       flex flex-col justify-center"
+       flex flex-col justify-center'
         >
-          <h1 className="text-4xl my-2 md:my-4 semibold">
+          <h1 className='text-4xl my-2 md:my-4 semibold'>
             {loading ? 'Processing...' : 'Login'}
           </h1>
-          <hr className="border-gray-600 py-1 w-full my-4" />
-          <label htmlFor="email" className="mt-3 mb-4">
+          <hr className='border-gray-600 py-1 w-full my-4' />
+          <label htmlFor='email' className='mt-3 mb-4'>
             Email
           </label>
           <input
-            className="p-2 rounded-md mb-4 border border-[#969696]"
-            id="email"
-            type="email"
+            className='p-2 rounded-md mb-4 border border-[#969696]'
+            id='email'
+            type='email'
             value={user.email}
             onChange={(event) =>
               setUser({ ...user, email: event.target.value })
             }
           />
-          <label htmlFor="password" className="mt-3 mb-4">
+          <label htmlFor='password' className='mt-3 mb-4'>
             Password
           </label>
           <input
-            className="p-2  rounded-md mb-4 border border-[#969696]"
-            id="password"
-            type="password"
+            className='p-2  rounded-md mb-4 border border-[#969696]'
+            id='password'
+            type='password'
             value={user.password}
             onChange={(event) =>
               setUser({ ...user, password: event.target.value })
             }
           />
-          <a href='/' className='text-lg  hover:underline cursor-pointer'>forgot password ?</a>
+          <a href='/' className='text-lg  hover:underline cursor-pointer'>
+            forgot password ?
+          </a>
           {!buttonDisabled ? (
             <button
               onClick={onLogin}
-              className="py-2 px-8 my-6 border text-center rounded-md 
+              className='py-2 px-8 my-6 border text-center rounded-md 
           transition duration-300 bg-blue-600 text-white hover:bg-blue-800
-      "
+      '
             >
               Login
             </button>
           ) : (
             <p
-              className="py-2 px-8 my-6 border text-center rounded-md text-gray-500 transition duration-300 bg-gray-200
-        "
+              className='py-2 px-8 my-6 border text-center rounded-md text-gray-500 transition duration-300 bg-gray-200
+        '
             >
               Login
             </p>
           )}
-          <Link href={'/signup'} className="hover:underline" target="_blank">
+          <Link href={'/signup'} className='hover:underline' target='_blank'>
             New User? Signup
           </Link>
         </div>
