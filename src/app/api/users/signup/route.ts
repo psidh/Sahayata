acca4385afde521de4ea53e722bcc93d295c737a
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-03-27 23:23:35 +0530

    removed email verification

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -37,9 +37,6 @@ export async function POST(request: NextRequest) {
     const savedUser = await newUser.save();
     console.log(savedUser);
 
-    //send verification email
-
-    await sendEmail({ email, emailType: 'VERIFY', userId: savedUser._id });
 
     return NextResponse.json({
       message: 'User created successfully',
